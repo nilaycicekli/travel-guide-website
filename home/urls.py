dbf4a5a7f5c1ffe913e1aae6d272957aac1f18e9
nilaycicekli <nilaycicekli@gmail.com>
2020-12-24 02:21:41 +0300

    frontend done except search_results. url's working.

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views 
 
 urlpatterns=[
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('home',views.home,name="home"),
+    path('home2',views.home2,name="home2"),
+    path('login',views.login,name="login"),
+    path('register',views.register,name="register"),
+    path('profile',views.profile,name="profile"),
+    path('profile/edit_profile',views.edit_profile,name="edit_profile"),
+    path('search_result',views.search_result,name="search_result"),
 ]
 
--- home/views.py
+++ home/views.py
@@ -4,3 +4,28 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def home2(request):
+    return render(request,'home2.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def search_result(request):
+    return render(request,'search_result.html')
+
+def profile(request):
+    return render(request,'profile.html')
+
+def edit_profile(request):
+    return render(request,'edit_profile.html')
+
+
+
+

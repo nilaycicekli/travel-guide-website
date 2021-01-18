0119b7a02cc2494a55a8bf2b52303ca929d0bb3d
dogukandoganay <d.doganay99@gmail.com>
2021-01-18 16:50:48 +0300

    minor update

--- blog/settings.py
+++ blog/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'd.doganay99@gmail.com'
-EMAIL_HOST_PASSWORD = '9878323748Dd:'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''

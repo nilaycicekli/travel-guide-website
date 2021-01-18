97f68972b5d2a4e6cbbc55e6730d0aec95200258
dogukandoganay <d.doganay99@gmail.com>
2021-01-18 07:53:51 +0300

    reset - change pwd

--- pprofile/urls.py
+++ pprofile/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
 from . import views 
 
 urlpatterns=[
@@ -8,5 +11,11 @@ urlpatterns=[
     path('saved',views.saved,name="saved"),
     path('published',views.published,name="published"),
     
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), 
+        name='password_change_done'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), 
+        name='password_change'),
+
 ]
 

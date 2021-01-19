e881fb40b08837a7212a659f23cd45369735b776
nilaycicekli <nilaycicekli@gmail.com>
2021-01-19 14:40:00 +0300

    new email address

--- blog/settings.py
+++ blog/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'xpleau@gmail.com'
+EMAIL_HOST_PASSWORD = 'explorewithus123.'
